e3ffea491a3e7cb4360d011055b841c8d76fd6bd
Muhammad Awaidus Attari <Muhammad Awaidus Attari>
2016-10-22 22:32:17 +0500

    start

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,23 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/register', function(req, res, next) {
+  var vm = {title: 'Register User'}
+  res.render('users/register', vm);
+});
+
+router.post('/register', function(req, res, next) {
+  var err = true; 
+  if (err) {
+  	var vm = {
+  		title: 'Register User',
+  		input: req.body,
+  		error: 'something went wrong'
+  	}  	
+  	return res.render('users/register', vm);	
+  }
+  res.redirect('/')
+  
+});
+
 module.exports = router;
