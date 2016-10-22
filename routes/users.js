870745d2a8893dbd674b9c0544e54b3f3a97d334
Muhammad Awaidus Attari <Muhammad Awaidus Attari>
2016-10-23 00:15:46 +0500

    23-06/12:15:pm

--- routes/users.js
+++ routes/users.js
@@ -1,28 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var userService = require('../services/user-service');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get('/register', function(req, res, next) {
-  var vm = {title: 'Register User'}
+router.get('/register', function (req, res, next) {
+  var vm = { title: 'Register User' }
   res.render('users/register', vm);
 });
 
-router.post('/register', function(req, res, next) {
-  var err = true; 
-  if (err) {
-  	var vm = {
-  		title: 'Register User',
-  		input: req.body,
-  		error: 'something went wrong'
-  	}  	
-  	return res.render('users/register', vm);	
-  }
-  res.redirect('/')
-  
+router.post('/register', function (req, res, next) {
+  userService.addUser(req.body, function (err) {
+    if (err) {
+      var vm = {
+        title: 'Register User',
+        input: req.body,
+        error: 'something went wrong'
+      }
+      return res.render('users/register', vm);
+    }
+    res.redirect('/')
+  });
 });
 
 module.exports = router;
